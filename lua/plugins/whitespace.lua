02a348066b22588f70b817be2cac2e714370f50d
Pisanu Boonyaanan <peat2512@hotmail.com>
2024-06-14 16:06:12 +0700

    Edit colorscheme and whitespace plugins

--- lua/plugins/whitespace.lua
+++ lua/plugins/whitespace.lua
@@ -1,11 +1,11 @@
 return {
   "jdhao/whitespace.nvim",
   event = "VimEnter",
-
-  vim.keymap.set(
-    "n",
-    "<S-F12>",
-    "<Cmd>StripTrailingWhitespace<CR>",
-    { noremap = true, silent = true, desc = "Remove Trailing Whitespace" }
-  ),
+  keys = {
+    {
+      "<S-F12>",
+      "<cmd>StripTrailingWhitespace<cr>",
+      { mode = { "n" }, noremap = true, desc = "Remove Trailing Whitespace" },
+    },
+  },
 }
