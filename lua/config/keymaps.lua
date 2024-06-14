e14e3e9776c18ba76ce262a0435fad2d4f40efba
Pisanu Boonyaanan <peat2512@hotmail.com>
2024-06-14 12:54:33 +0700

    Add plugins and bindings for tmux

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,7 @@ keymap.set("n", "x", '"_x', { noremap = true, silent = true, desc = "Delete sing
 keymap.set("n", "<C-a>", "gg<S-v>G", { noremap = true, silent = true, desc = "Select All" })
 keymap.set("n", "+", "<C-a>", { noremap = true, silent = true, desc = "Increment" })
 keymap.set("n", "-", "<C-x>", { noremap = true, silent = true, desc = "Decrement" })
+keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", { noremap = true, silent = true, desc = "Window left" })
+keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { noremap = true, silent = true, desc = "Window right" })
+keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { noremap = true, silent = true, desc = "Window down" })
+keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { noremap = true, silent = true, desc = "Window up" })
