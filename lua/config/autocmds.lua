a29468431e25e188a19cf4b0d797b002e2aa7167
Pisanu Boonyaanan <peat2512@hotmail.com>
2024-11-27 13:51:10 +0700

    Disable cmdline UI and edit some description

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,7 +6,7 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
--- Remove comment on newlining (currently not working)
+-- Remove comment on newlining
 vim.api.nvim_create_autocmd("BufEnter", {
   group = augroup("no_comment_on_newline"),
   callback = function()
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    cmdline = {
+      enabled = true,
+      view = "cmdline", -- classic version (cmdline_popup for lazyvim style)
+    },
+  },
+}
